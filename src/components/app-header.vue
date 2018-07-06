90ff4cae6efa772da3703760d6d671eb8935376f
Victor Kozlov <punkrus18@gmail.com>
2018-07-06 13:16:04 +0300

    concept
    add: header fields for params;

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <label>
-            Date:
-            <input type="date" v-model="date"/>
-        </label>
+    <div class="app-header">
+        <div>
+            <label>
+                Date:
+                <input type="date" v-model="date"/>
+            </label>
+            {{2}}/{{3}}
+        </div>
+        <div>
+            <label>
+                Weight:
+                <input type="number"/>
+            </label>
+            <label>
+                Chest:
+                <input type="number"/>
+            </label>
+            <label>
+                Waist:
+                <input type="number"/>
+            </label>
+            <label>
+                Thighs:
+                <input type="number"/>
+            </label>
+        </div>
     </div>
 </template>
 
 <script>
-import Vue from 'vue'
-
 export default {
   name: 'AppHeader',
   data: function () {
@@ -26,5 +45,10 @@ export default {
 </script>
 
 <style scoped>
-
+    .app-header {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
