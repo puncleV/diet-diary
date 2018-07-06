589533b89e8b62a867a54e5d0e3877147f31b5c4
Victor Kozlov <punkrus18@gmail.com>
2018-07-06 15:20:07 +0300

    daily list
    add: food list;
    add: groups for food lists;

--- src/components/daily-list.vue
+++ src/components/daily-list.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        Hello world!
+        <food-list v-for="group in groups" :key="group" :group="group"></food-list>
     </div>
 </template>
 
 <script>
+import FoodList from './food-list'
 export default {
   name: 'DailyList',
-  props: ['selectedDate']
+  components: {FoodList},
+  props: ['selectedDate'],
+  data: function () {
+    return {
+      groups: [
+        'breakfast',
+        'morningSnack',
+        'lunch',
+        'eveningSnack',
+        'dinner',
+        'lateSnack'
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/food-list.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            food list {{group}}
+        </div>
+        <table>
+            <thead>
+            <tr><th>a</th></tr>
+            </thead>
+            <tbody>
+            <tr><td>a</td></tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FoodList',
+  props: ['group']
+}
+</script>
+
+<style scoped>
+
+</style>
